4ec330c269974585e76d0543616a223b552a131e
Thaís <thcba@cesar.school>
2024-05-21 19:49:18 -0300

    comentários em keyboard.h e keyboard.c

--- cli-lib/include/keyboard.h
+++ cli-lib/include/keyboard.h
@@ -3,17 +3,18 @@
  * Created on Aug, 23th 2023
  * Author: Tiago Barros
  * Based on "From C to C++ course - 2002"
+ * Declarações das funções para controle de entrada do teclado.
 */
 
 #ifndef __KEYBOARD_H__
 #define __KEYBOARD_H__
 
-void keyboardInit();
+void keyboardInit(); // Inicializa a configuração do teclado para entrada não bloqueante
 
-void keyboardDestroy();
+void keyboardDestroy(); // Restaura as configurações originais do teclado
     
-int keyhit();
+int keyhit(); // Verifica se uma tecla foi pressionada
 
-int readch();
+int readch(); // Lê o caractere pressionado
 
 #endif /* __KEYBOARD_H__ */
--- cli-lib/src/keyboard.c
+++ cli-lib/src/keyboard.c
@@ -1,8 +1,9 @@
 /**
- * keyboard.h
+ * keyboard.c
  * Created on Aug, 23th 2023
  * Author: Tiago Barros
  * Based on "From C to C++ course - 2002"
+ * Implementação das funções para controle de entrada do teclado utilizando <termios.h> para manipulação do terminal
 */
 
 #include <termios.h>
@@ -10,59 +11,63 @@
 
 #include "keyboard.h"
 
+// Variáveis estáticas para armazenar configurações do terminal
 static struct termios initialSettings, newSettings;
-static int peekCharacter;
+static int peekCharacter; // Armazena o próximo caractere lido (buffer)
 
-
+// Inicializa a configuração do teclado para entrada não bloqueante
 void keyboardInit()
 {
-    tcgetattr(0,&initialSettings);
-    newSettings = initialSettings;
-    newSettings.c_lflag &= ~ICANON;
-    newSettings.c_lflag &= ~ECHO;
-    newSettings.c_lflag &= ~ISIG;
-    newSettings.c_cc[VMIN] = 1;
-    newSettings.c_cc[VTIME] = 0;
-    tcsetattr(0, TCSANOW, &newSettings);
+    tcgetattr(0, &initialSettings);  // Obtém as configurações atuais do terminal
+    newSettings = initialSettings;   // Copia as configurações atuais
+    newSettings.c_lflag &= ~ICANON;  // Desativa o modo canônico
+    newSettings.c_lflag &= ~ECHO;    // Desativa a exibição dos caracteres digitados
+    newSettings.c_lflag &= ~ISIG;    // Desativa sinais de interrupção (Ctrl+C, Ctrl+Z)
+    newSettings.c_cc[VMIN] = 1;      // Mínimo de caracteres para leitura
+    newSettings.c_cc[VTIME] = 0;     // Tempo de espera para leitura (desativado)
+    tcsetattr(0, TCSANOW, &newSettings); // Aplica as novas configurações imediatamente
 }
 
+// Restaura as configurações originais do teclado
 void keyboardDestroy()
 {
-    tcsetattr(0, TCSANOW, &initialSettings);
+    tcsetattr(0, TCSANOW, &initialSettings); // Restaura as configurações originais
 }
 
+// Verifica se uma tecla foi pressionada
 int keyhit()
 {
     unsigned char ch;
     int nread;
 
-    if (peekCharacter != -1) return 1;
+    if (peekCharacter != -1) return 1; // Se há um caractere armazenado, retorna verdadeiro
     
-    newSettings.c_cc[VMIN]=0;
+    newSettings.c_cc[VMIN]=0; // Muda para modo não bloqueante
     tcsetattr(0, TCSANOW, &newSettings);
-    nread = read(0,&ch,1);
-    newSettings.c_cc[VMIN]=1;
+    nread = read(0,&ch,1); // Tenta ler um caractere
+    newSettings.c_cc[VMIN]=1; // Restaura o modo de leitura
     tcsetattr(0, TCSANOW, &newSettings);
     
     if(nread == 1) 
     {
-        peekCharacter = ch;
-        return 1;
+        peekCharacter = ch; // Armazena o caractere lido
+        return 1; // Retorna verdadeiro se um caractere foi lido
     }
     
-    return 0;
+    return 0; // Retorna falso se nenhum caractere foi lido
 }
 
+// Lê o caractere pressionado
 int readch()
 {
     char ch;
 
-    if(peekCharacter != -1)
+    if (peekCharacter != -1)
     {
-        ch = peekCharacter;
-        peekCharacter = -1;
-        return ch;
+        ch = peekCharacter;          // Usa o caractere armazenado se disponível
+        peekCharacter = -1;          // Reseta o buffer
+        return ch;                   // Retorna o caractere lido
     }
-    read(0,&ch,1);
-    return ch;
+    read(0, &ch, 1);                 // Lê um caractere do terminal
+    return ch;                       // Retorna o caractere lido
 }
