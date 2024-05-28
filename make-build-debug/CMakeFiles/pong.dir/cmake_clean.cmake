77e59caab788ca8fcf235a3a9ec763354749e53f
Thaís <thcba@cesar.school>
2024-05-28 09:33:32 -0300

    testando o jogo

---
+++ make-build-debug/CMakeFiles/pong.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pong.dir/cli-lib/src/keyboard.c.o"
+  "CMakeFiles/pong.dir/cli-lib/src/keyboard.c.o.d"
+  "CMakeFiles/pong.dir/cli-lib/src/screen.c.o"
+  "CMakeFiles/pong.dir/cli-lib/src/screen.c.o.d"
+  "CMakeFiles/pong.dir/cli-lib/src/timer.c.o"
+  "CMakeFiles/pong.dir/cli-lib/src/timer.c.o.d"
+  "CMakeFiles/pong.dir/main.c.o"
+  "CMakeFiles/pong.dir/main.c.o.d"
+  "pong"
+  "pong.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pong.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
